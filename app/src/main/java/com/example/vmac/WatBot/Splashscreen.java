9e8443223595a73a84326632c1b56ff16cb20af3
PoojaK97 <thisispooja97@gmail.com>
2019-11-05 12:49:55 +0530

    Splashscreen

---
+++ app/src/main/java/com/example/vmac/WatBot/Splashscreen.java
@@ -0,0 +1,37 @@
+package com.example.vmac.WatBot;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class Splashscreen extends AppCompatActivity {
+
+    private int SLEEP_TIMER = 3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Log.d("splash","splash screen invoked");
+        setContentView(R.layout.activity_splashscreen);
+        LogoLauncher logoLauncher = new LogoLauncher();
+        logoLauncher.start();
+
+    }
+
+    private class LogoLauncher extends Thread {
+
+        public void run(){
+
+            try{
+                sleep(1000 * SLEEP_TIMER);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            Intent intent = new Intent(Splashscreen.this,MainActivity.class);
+            startActivity(intent);
+            Splashscreen.this.finish();
+        }
+    }
+}
